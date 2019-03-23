6156923e512fcf0d1059211c640adf18a37b867b
tim miller <tmiller12@gmail.com>
2019-03-23 11:31:02 -0600

    Sorted clues into three arrays roundOne, RoundTwo and finalRound
    Co-authored-by: Matthew Kaplan <kaplan.matthew.p@gmail.com>

--- src/Game.js
+++ src/Game.js
@@ -6,16 +6,20 @@ class Game {
   constructor() {
     this.players = [];
     this.allData = [[], [], [], [], [], [], [], [], [], []];
+    this.roundOne = [];
+    this.roundTwo = [];
+    this.finalRound = [];
   }
   startGame() {
     domUpdates.hideStartScreen();
     let names = domUpdates.grabNames();
     this.players.push(names);
     this.getRandomData();
+    this.sortRounds();
   }
 
 
-  getRandomData () {
+  getRandomData() {
     this.allData.forEach((cat, ind) => {
       data.clues.forEach(clue => { 
         if (clue.categoryId === (ind + 1)) {
@@ -27,9 +31,23 @@ class Game {
     console.log(this.allData);
   }
 
-  shuffle (a) {
+  shuffle(a) {
     return a.sort(() => 0.5 - Math.random());
   }
+
+  sortRounds() {
+    const roundArr = this.allData
+
+    this.roundOne.categoryIds = roundArr.splice(0, 4);
+    this.roundTwo.categoryIds = roundArr.splice(0, 4);
+    this.finalRound.categoryIds = roundArr.splice(0, 1);
+
+    console.log(this.roundOne);
+    console.log(this.roundTwo);
+    console.log(this.finalRound);
+    console.log(this.allData);
+  }
+
 
 }
 
