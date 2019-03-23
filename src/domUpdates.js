435a1fe5a7f52677d5a5217b1e1ab4307981b6eb
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-03-23 12:11:37 -0600

    question display
    Co-authored-by: Tim Miller <TMiller12@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,10 +1,8 @@
 import $ from 'jquery';
 import Categories from './Categories.js';
-import Round from './Round.js';
 import Game from './Game.js';
 
 let categories = new Categories();
-let round = new Round();
 
 export default {
 
@@ -21,14 +19,14 @@ export default {
     $('.player-3-name').text($players[2]);
     return $players;
   },
+  
   displayCategories() {
     categories.grabCategories();
     const topics = categories.categoriesArr;
-    console.log(topics);
     $(`.cat-${0}`).text(topics[0]);
     $(`.cat-${1}`).text(topics[1]);
     $(`.cat-${2}`).text(topics[2]);
     $(`.cat-${3}`).text(topics[3]);
-
+    console.log(topics);
   },
 }
