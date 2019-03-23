460aca49ce31fa9b56b920256834a56e186d7375
tim miller <tmiller12@gmail.com>
2019-03-23 10:43:23 -0600

    Changed method to grab data - setting clue data to 10 arrays for each catagory - next step is to sort data into round 1 and round 2 array

--- src/Game.js
+++ src/Game.js
@@ -5,21 +5,32 @@ import Question from './Question.js';
 class Game {
   constructor() {
     this.players = [];
-    this.allQuestions = [];
+    this.allData = [[], [], [], [], [], [], [], [], [], []];
   }
   startGame() {
     domUpdates.hideStartScreen();
     let names = domUpdates.grabNames();
     this.players.push(names);
-    this.createQuestions();
+    this.getRandomData();
   }
 
-  createQuestions() {
-    Object.values(data.clues).forEach(clue => {
-      this.allQuestions.push(new Question(clue.question, clue.pointValue, clue.answer, clue.potentialAnswers, clue.categoryId));
+
+  getRandomData () {
+    this.allData.forEach((cat, ind) => {
+      data.clues.forEach(clue => { 
+        if (clue.categoryId === (ind + 1)) {
+          cat.push(clue);
+        }
+      })
     });
-    console.log(this.allQuestions);
+    this.shuffle(this.allData).forEach(cat => this.shuffle(cat));
+    console.log(this.allData);
   }
+
+  shuffle (a) {
+    return a.sort(() => 0.5 - Math.random());
+  }
+
 }
 
 
