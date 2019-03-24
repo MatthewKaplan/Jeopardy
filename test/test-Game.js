ebefc989ef01a26765f58480564cf9f524bbc7ba
tim miller <tmiller12@gmail.com>
2019-03-24 11:28:02 -0600

    started testing game class
    Co-authored-by: Matthew Kaplan <kaplan.matthew.p@gmail.com>

--- test/test-Game.js
+++ test/test-Game.js
@@ -1,2 +1,38 @@
 import chai from 'chai';
+import Game from '../Game.js';
+import data from '../data.js';
+import spies from 'chai-spies';
+chai.use(spies);
+
 const expect = chai.expect;
+
+
+
+describe('Game', () => {
+  let game;
+   
+  beforeEach(() => {
+    game = new Game();
+  })
+
+
+  it('should be an instance of Game', () => {
+    expect()
+  })
+
+  it('should have default properties', () => {
+
+
+  })
+
+  it('should have three player objs', () => {
+
+  })
+
+  it('should gather all data on start', () => {
+
+  }
+
+
+
+})
